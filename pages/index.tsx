5f010d78b3ec02872953ff579776dbc0bc932399
Claudson Martins <c-claudson.santos@integrate.com>
2022-10-11 08:26:23 -0300

    first

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+import HeroSection, { HeroSectionSiblings } from "../components/Sections/HeroSection";
+import FeaturesSection from "../components/Sections/FeaturesSection";
+import CTASection from "../components/Sections/CTASection";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Dummy Next.js Project</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div className="relative overflow-hidden bg-white">
+                <div className="mx-auto max-w-7xl">
+                    <HeroSection/>
+                </div>
+                <HeroSectionSiblings />
+            </div>
+            <FeaturesSection/>
+            <CTASection />
+        </>
+    )
+}
+
+export default Home
